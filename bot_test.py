63b14e1d51997c101ef4935b6187a8ff18ace2a3
XeniusDZ <76628837+XeniusDZ@users.noreply.github.com>
2021-12-18 11:40:58 +0300

    Project finished

--- bot_test.py
+++ bot_test.py
@@ -4,6 +4,7 @@ import bot
 
 
 class MyTestCase(unittest.TestCase):
+    #testing if we get an image in answer if we give a question that has a picture and one which doesn't
     def test_question(self):
         data = bot.question("youtube")
         self.assertEqual("image" in data, True)
